d59abe462e3e6ef06a9b55f5141fcc1df95f1f17
Bhavana Rana <4bhavanarana@gmail.com>
2022-05-06 10:41:59 +0530

    challenge to show onsale by using boolean

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 const app = Vue.createApp({
-  //vue access to through installation, create our vue app
-  data: function () {
+  //we have acess vue through installation, create our vue app
+  data() {
     // In argument we pass data property which is a function
     return {
       product: "Socks", // here we have store our data
       image: "/Images/blue-socks.webp",
       //   inStock: true
       productItem: 100,
+      onSale: true
     };
   },
 });
