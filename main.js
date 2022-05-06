592d7bb873de334cac36bd69ea50273a34bfae89
Bhavana Rana <4bhavanarana@gmail.com>
2022-05-06 11:03:45 +0530

    formatted files

--- main.js
+++ main.js
@@ -1,19 +1,15 @@
 const app = Vue.createApp({
-  //we have acess vue through installation, create our vue app
   data() {
-    // In argument we pass data property which is a function
     return {
-      product: "Socks", // here we have store our data
+      product: "Socks",
       image: "/Images/blue-socks.webp",
-      //   inStock: true
-      productItem: 100,
-      onSale: true,
-      details: ['50% cotton', '30% wool', '20% polyester'],
-      sizes: ['S', 'M', 'L', 'XL'],
-      varients: [
-        {id: 2234, color: 'green'},
-        {id: 2235, color: 'blue'},
-      ]
+      inStock: true,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      sizes: ["S", "M", "L", "XL"],
+      variants: [
+        { id: 2234, color: "green" },
+        { id: 2235, color: "blue" },
+      ],
     };
   },
 });
