36d70ae41f48fe87b7c1135bff079bae3a3a6e76
Bhavana Rana <4bhavanarana@gmail.com>
2022-05-08 22:14:08 +0530

    disable add and delete button at certain condition

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const app = Vue.createApp({
       product: "Socks",
       brand: "Vue Mastery",
       image: "/Images/blue-socks.webp",
-      inStock: false,
+      inStock: true,
       details: ["50% cotton", "30% wool", "20% polyester"],
       variants: [
         { id: 2234, color: "green", image: "/Images/green-socks.webp" },
